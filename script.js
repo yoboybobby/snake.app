02834378a2ef86cacbcc2e4b3161edb46f7c3386
yoboybobby <ssproo967@gmail.com>
2026-06-29 12:22:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function number(){
+    let num = prompt("enter a number")
+    if (num * num % 2 == 0){
+        document.writeln("זוגי");
+    }
+    else{
+       document.writeln("אי זוגי ");
+    }
+    
+}
